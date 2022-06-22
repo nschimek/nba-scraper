71ea649729e8e855f004908e8bd239e3f0c82759
Nick Schimek <schimek@gmail.com>
2022-06-21 22:07:41 -0500

    cleanup

--- parser/player.go
+++ parser/player.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -18,9 +17,9 @@ const (
 	bornDateRegex       = `Born:\s+(?P<birthMonth>\w+)\s+(?P<birthDay>\d{1,2}),\s+(?P<birthYear>\d{4})\s+in.(?P<birthPlace>.+)\s\s(?P<birthCountry>[a-z]{2})$`
 )
 
-type regexAssign struct {
-	regex    string
-	assigner func(m *model.Player, rm map[string]string)
+type regexParser struct {
+	regex  string
+	parser func(m *model.Player, rm map[string]string)
 }
 
 func parseShootsPosition(p *model.Player, rm map[string]string) {
@@ -35,32 +34,31 @@ func parseHeightWeight(m *model.Player, rm map[string]string) {
 }
 
 func parseBirthInfo(p *model.Player, rm map[string]string) {
-	fmt.Println(rm)
 	p.BirthDate, _ = time.Parse("January 2 2006", rm["birthMonth"]+" "+rm["birthDay"]+" "+rm["birthYear"])
 	p.BirthPlace = strings.TrimSpace(rm["birthPlace"])
 	p.BirthCountryCode = strings.ToUpper(rm["birthCountry"])
 }
 
-var regexAssigners = [...]regexAssign{
-	{regex: positionShootsRegex, assigner: parseShootsPosition},
-	{regex: heightWeightRegex, assigner: parseHeightWeight},
-	{regex: bornDateRegex, assigner: parseBirthInfo},
+var regexParsers = [...]regexParser{
+	{regex: positionShootsRegex, parser: parseShootsPosition},
+	{regex: heightWeightRegex, parser: parseHeightWeight},
+	{regex: bornDateRegex, parser: parseBirthInfo},
 }
 
 func (p *PlayerParser) PlayerInfoBox(m *model.Player, div *colly.HTMLElement) {
 	m.Name = div.ChildText("h1")
 
-	cra := 0 // current regex assigner starts at 0
-	max := len(regexAssigners)
+	crp := 0 // current regex parser starts at 0
+	max := len(regexParsers)
 
 	div.ForEach("p", func(i int, e *colly.HTMLElement) {
 		line := strings.TrimSpace(removeNewlines(e.Text))
-		if cra < max {
-			rm := RegexParamMap(regexAssigners[cra].regex, line)
-			// if there's a hit with this regex, we want to run the assigner function and increment cra
+		if crp < max {
+			rm := RegexParamMap(regexParsers[crp].regex, line)
+			// if there's a hit with this regex, we want to run the parser function and increment cra
 			if len(rm) > 0 {
-				regexAssigners[cra].assigner(m, rm)
-				cra++
+				regexParsers[crp].parser(m, rm)
+				crp++
 			}
 			// the regex list is in the same order as the elements are listed on the page... so if we do not get a hit,
 			// we simply do nothing and move on to the next line.  the next matching line will come soon enough.
