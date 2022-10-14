6b3ceecf5eec44eb3e0ce2814eb59751ec65a955
Nick Schimek <schimek@gmail.com>
2022-10-13 22:03:07 -0500

    build script update

--- go-build.sh
+++ go-build.sh
@@ -6,3 +6,4 @@ zip -r nba-scraper-linux-${VERSION}.zip nba-scraper config/sample.yaml
 echo "Building and zipping Windows binary ${VERSION}..."
 GOOS=windows GOARCH=amd64 go build -ldflags "-X 'github.com/nschimek/nba-scraper/core.Version=${VERSION}'"
 zip -r nba-scraper-win-${VERSION}.zip nba-scraper.exe config/sample.yaml
+git push origin ${VERSION}
